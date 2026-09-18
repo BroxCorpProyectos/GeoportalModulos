6eec6659586fc3b20cc0ede0d21563504a83abce
BroxCorpProyectos <brocxcorpproyectos@gmail.com>
2026-09-18 01:27:18 -0500

    Update app.js

--- app.js
+++ app.js
@@ -110,14 +110,19 @@ function inicializarMapa() {
     // SATÉLITE
     // --------------------------------------------------------
     
-    const capaSatelite = L.tileLayer(
-        "https://global.imagery.hotosm.org/{z}/{x}/{y}.png",
-        {
-            maxZoom: 22,
-            attribution:
-                '&copy; OpenAerialMap contributors'
-        }
-    );
+    const MAPTILER_KEY = "66uMzDVzttzlPmbt4O6o";
+
+    capaSatelite =
+        L.tileLayer(
+            `https://api.maptiler.com/maps/satellite/{z}/{x}/{y}.jpg?key=${MAPTILER_KEY}`,
+            {
+                maxZoom: 22,
+    
+                attribution:
+                    '&copy; MapTiler &copy; OpenStreetMap contributors'
+            }
+        );
+
 
 
     // --------------------------------------------------------
